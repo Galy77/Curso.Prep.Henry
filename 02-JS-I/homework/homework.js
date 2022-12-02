d7800f393b0b8843f6903615b97c3ea89e1bc750
Diego <diegogaleano980@gmail.com>
2022-12-02 15:37:13 -0300

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = "Argentina";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 325.45;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 35;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
